938bbc5aa382f3c3bb35ab51734cc2c10b229b29
alesandramempis <alesandramempis@gmail.com>
2022-06-06 14:26:24 -0300

    Evento do teclado

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ const teclado = {
     esquerda: false, 
     direita: false, 
 }
+
+const jogador = new Pad(ctx, teclado)
+const bola = new Bola(ctx, jogador)
+
+
+window.addEventListener('keydown', (event) => {
+    if(event.keyCode == 37) {
+        teclado.esquerda = true
+    } else if (event.keyCode == 39) {
+        teclado.direita = true
+    }
+
+    if(event.keyCode == 38) {
+        teclado.cima = true
+    } else if (event.keyCode == 40) {
+        teclado.baixo = true
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    if(event.keyCode == 37) {
+        teclado.esquerda = false
+    } else if (event.keyCode == 39) {
+        teclado.direita = false
+    }
+    if(event.keyCode == 38) {
+        teclado.cima = false
+    } else if (event.keyCode == 40) {
+        teclado.baixo = false
+    }
+})
