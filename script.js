cb1d47d676234e2bbe82c6d56a333993bb5fdf1e
alesandramempis <alesandramempis@gmail.com>
2022-06-06 14:24:27 -0300

    Criação do jogador

---
+++ script.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById('desenho1');
+let ctx = canvas.getContext('2d');
+
+const teclado = {
+    cima: false,
+    baixo: false, 
+    esquerda: false, 
+    direita: false, 
+}
